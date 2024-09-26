540acc58261568377d07736e7d2fe8f3d8f16524
hun0219 <gsw98000@gmail.com>
2024-09-26 10:06:18 +0900

    random

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import os
+import random
 
 app = FastAPI()
 
+
+def predict():
+    dog_list = ["hotdog","not hotdog"]
+
+    return dog_list[random.randint(0,1)]
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "hotdog"}
+    dog = predict()
+    return dog
